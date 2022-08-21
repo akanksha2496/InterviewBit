a68b9fc80fcd1aaa90ae367a77a9b9f781e97f8e
Akanksha Dewangan <59597850+akanksha2496@users.noreply.github.com>
2022-08-21 18:34:09 +0530

    Create EvaluateExpression.java

---
+++ StackAndQueue/EvaluateExpression.java
@@ -0,0 +1,102 @@
+Evaluate Expression
+
+Bookmark
+Easy
+55.4% Success
+
+162
+
+13
+Asked In:
+Yahoo
+Google
+Facebook
+Problem Description
+ 
+ 
+
+An arithmetic expression is given by a charater array A of size N. Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, /. Each character may be an integer or an operator.
+
+
+
+Problem Constraints
+1 <= N <= 105
+
+
+
+Input Format
+The only argument given is character array A.
+
+
+
+Output Format
+Return the value of arithmetic expression formed using reverse Polish Notation.
+
+
+
+Example Input
+Input 1:
+    A =   ["2", "1", "+", "3", ""]
+Input 2:
+    A = ["4", "13", "5", "/", "+"]
+
+
+Example Output
+Output 1:
+    9
+Output 2:
+    6
+
+
+Example Explanation*
+Explaination 1:
+    starting from backside:
+    * : () * ()
+    3 : () * (3)
+    + : (() + ()) * (3)
+    1 : (() + (1)) * (3)
+    2 : ((2) + (1)) * (3)
+    ((2) + (1)) * (3) = 9
+Explaination 2:
+    + : () + ()
+    / : () + (() / ())
+    5 : () + (() / (5))
+    13 : () + ((13) / (5))
+    4 : (4) + ((13) / (5))
+    (4) + ((13) / (5)) = 6
+    
+    
+    code:
+    import java.util.Stack;
+
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        int a,b;
+        Stack<Integer> S = new Stack<Integer>();
+        for (String s : tokens) {
+            if(s.equals("+")) {
+                S.add(S.pop()+S.pop());
+            }
+            else if(s.equals("/")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a / b);
+            }
+            else if(s.equals("*")) {
+                S.add(S.pop() * S.pop());
+            }
+            else if(s.equals("-")) {
+                b = S.pop();
+                a = S.pop();
+                S.add(a - b);
+            }
+            else {
+                S.add(Integer.parseInt(s));
+            }
+        }    
+        return S.pop();
+    }
+}
+
